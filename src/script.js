ba2c54f77b362fe92eeaf550ed08a4e8fc6c81b9
TonyNguyen137 <tonynguyen137@yahoo.de>
2023-05-24 08:25:33 +0200

    first commit

---
+++ src/script.js
@@ -0,0 +1,12 @@
+function tweetQuote() {
+    let text ="Css is awesome =))"
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${text} - Tony`;
+
+    window.open(twitterUrl, "_blank");
+}
+
+let twitterIcons = document.querySelectorAll(".twitterButton");
+
+twitterIcons.forEach( e => {
+    e.addEventListener("click", tweetQuote);
+})
